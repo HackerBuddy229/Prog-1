79d87cf04ae2f3ad926654b275af4fa62edb5138
HackerBuddy229 <rasmusb229@gmail.com>
2019-09-19 13:31:26 +0200

    Wrote java docs

--- homework/v.38/src/com/TE18C/Main.java
+++ homework/v.38/src/com/TE18C/Main.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 
 public class Main extends Methods {
     /**
-     *
-     * @param args
+     *Main method
+     * @param args not utilized
      */
     public static void main(String[] args) {
 	// write your code here
--- homework/v.38/src/com/TE18C/Methods.java
+++ homework/v.38/src/com/TE18C/Methods.java
@@ -6,10 +6,10 @@ import java.util.Scanner;
 
 public class Methods {
     /**
-     *
-     * @param min
-     * @param max
-     * @param avg
+     * Prints information regarding the highest number, the lowest number and the average to the console.
+     * @param min an integer representing the lowest number
+     * @param max an integer representing the highest number
+     * @param avg a double representing the average
      */
     protected static void Display(int min, int max, double avg) {
         System.out.println("Högst: " + max);
@@ -18,9 +18,9 @@ public class Methods {
     }
 
     /**
-     *
-     * @param integerInput
-     * @return
+     * uses integerInput to resolve and return the average of the arrayList
+     * @param integerInput an arraylist consisting of integers
+     * @return returns the average of the integerInput arraylist in the form of a double
      */
     protected static double SetAvg(ArrayList<Integer> integerInput) {
         int total = 0;
@@ -37,9 +37,9 @@ public class Methods {
     }
 
     /**
-     *
-     * @param integerInput
-     * @return
+     * Uses integerInput to resolve and return the highest number existing in the arraylist
+     * @param integerInput an arraylist consisting of integers
+     * @return returns an integer that represents the highest entry in the arraylist
      */
     protected static int SetMax(ArrayList<Integer> integerInput) {
         int max = 0;
@@ -55,9 +55,9 @@ public class Methods {
     }
 
     /**
-     *
-     * @param integerInput
-     * @return
+     *Uses integerInput to resolve and return the lowest number existing in the arraylist
+     * @param integerInput an arraylist consisting of integers
+     * @return returns an integer that represents the lowest entry in the arraylist
      */
     protected static int SetMin(ArrayList<Integer> integerInput) {
         int min = integerInput.get(0);
@@ -74,9 +74,9 @@ public class Methods {
     }
 
     /**
-     *
-     * @param parsedInput
-     * @return
+     *Converts an array of strings to an arraylist of int
+     * @param parsedInput an array consisting of strings
+     * @return an arraylist of int
      */
     protected static ArrayList<Integer> ConvertToInt(String[] parsedInput) {
         ArrayList<Integer> IntegerInput = new ArrayList<>(parsedInput.length);
@@ -90,8 +90,8 @@ public class Methods {
     }
 
     /**
-     *
-     * @param parsedInput
+     *Prints out the content of an array of strings for debug purposes
+     * @param parsedInput an array of strings
      */
     private static void DebugPring(String[] parsedInput) {
         for (String x:parsedInput
@@ -101,17 +101,17 @@ public class Methods {
     }
 
     /**
-     *
-     * @param input
-     * @return
+     *Formats a string containing multiple inputs separated by " " into an array of strings
+     * @param input String to be broken up into an array
+     * @return  an array of strings
      */
     protected static String[] ParseInput(String input) {
         return input.split(" ");
     }
 
     /**
-     *
-     * @return
+     *Prints a prompt to the console and awaits a string
+     * @return formentioned string
      */
     protected static String GetInput() {
         System.out.print("Skriv in 10 heltal: ");
