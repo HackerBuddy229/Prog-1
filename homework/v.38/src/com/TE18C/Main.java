b9fa80717a10b5bfa2c65cf4bec1f41611841dfa
HackerBuddy229 <rasmusb229@gmail.com>
2019-09-19 08:44:38 +0200

    accepting and parsing input

--- homework/v.38/src/com/TE18C/Main.java
+++ homework/v.38/src/com/TE18C/Main.java
@@ -1,8 +1,25 @@
 package com.TE18C;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        String input = GetInput();
+        String[] parsedInput = ParseInput(input);
+
+    }
+
+    private static String[] ParseInput(String input) {
+        return input.split(" ");
+    }
+
+    private static String GetInput() {
+        System.out.print("Skriv in 10 heltal: ");
+        Scanner sc = new Scanner(System.in);
+        return sc.nextLine();
+
     }
 }
