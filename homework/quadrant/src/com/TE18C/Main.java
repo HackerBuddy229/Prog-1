3db38cdc77ef561cb0cd4511eb58a3ea5a3a4f6f
HackerBuddy229 <rasmusb229@gmail.com>
2019-09-26 10:45:09 +0200

    Final

--- homework/quadrant/src/com/TE18C/Main.java
+++ homework/quadrant/src/com/TE18C/Main.java
@@ -12,7 +12,32 @@ public class Main {
 
         Cordinate point = new Cordinate(x, y);
 
+        if (IsUp(point) && IsLeft(point)){
+            System.out.println("2");
+        } else if (IsUp(point) && !IsLeft(point)) {
+            System.out.println("1");
+        } else if (!IsUp(point) && IsLeft(point)) {
+            System.out.println("3");
+        } else {
+            System.out.println("4");
+        }
 
+    }
+
+    static boolean IsUp(Cordinate point) {
+        if (point.getY() > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    static boolean IsLeft(Cordinate point) {
+        if (point.getX() < 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
 
