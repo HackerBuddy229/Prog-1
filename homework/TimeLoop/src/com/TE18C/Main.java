566dac9a44d3fc828b44f61b15eb69eb46983c17
HackerBuddy229 <rasmusb229@gmail.com>
2019-09-26 10:56:19 +0200

    Final

--- homework/TimeLoop/src/com/TE18C/Main.java
+++ homework/TimeLoop/src/com/TE18C/Main.java
@@ -1,8 +1,15 @@
 package com.TE18C;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Scanner sc = new Scanner(System.in);
+        int input = Integer.parseInt(sc.nextLine());
+        for (int i = 1; i <= input; i++) {
+            System.out.println(i + " Abracadabra");
+        }
     }
 }
