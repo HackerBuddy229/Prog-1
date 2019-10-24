8f5506ec91792061a94f2f2ea200a1a4610d71bc
HackerBuddy229 <rasmusb229@gmail.com>
2019-10-24 10:02:04 +0200

    ageControll()

--- homework/v.43/src/com/TE18C/Methods.java
+++ homework/v.43/src/com/TE18C/Methods.java
@@ -39,8 +39,31 @@ public class Methods{
      * @return returns double as kelving
      */
     private static double kelvin(double _celcius){
+
         return _celcius + 273.15;
     }
+
+    private static void ageControll(int _age){
+        if (_age >= 0 && _age <= 5){
+            System.out.println("Småbarn får inte göra något.");
+        } else if(_age <= 12){
+            System.out.println("Du får spela Fortnite.");
+        } else if(_age <= 14){
+            System.out.println("Du är tonåring.");
+        } else if(_age <= 17){
+            System.out.println("Du får köra moppe.");
+        } else if(_age <= 20){
+            System.out.println("Du får köra bil.");
+        } else if(_age <= 64){
+            System.out.println("Du får vuxenstraff om du gör något dumt.");
+        } else if(_age <= 99){
+            System.out.println("Du är pensionär.");
+        } else {
+            System.out.println("R.I.P?");
+        }
+    }
+
+
 
 
 
