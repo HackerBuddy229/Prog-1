350f695984c1d3b125be1c58692d780df1abda76
HackerBuddy229 <rasmusb229@gmail.com>
2019-11-11 10:24:44 +0100

    constants

--- enumProj/src/com/TE18C/Main.java
+++ enumProj/src/com/TE18C/Main.java
@@ -2,7 +2,19 @@ package com.TE18C;
 
 public class Main {
 
+    public final double G = 0.00000000006674;
+    public final double R = 8.3145;
+    public final int p_0 = 1013;
+    public final int c = 299792458;
+    public final double g_swe = 9.84671;
+
     public static void main(String[] args) {
 	// write your code here
     }
+
+
+
+
+
+
 }
