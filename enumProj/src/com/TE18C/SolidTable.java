4e78962ba27ba8ff40c41a3beafc927107a9a8a1
HackerBuddy229 <rasmusb229@gmail.com>
2019-12-01 22:00:09 +0100

    Updated SolidTable

--- enumProj/src/com/TE18C/SolidTable.java
+++ enumProj/src/com/TE18C/SolidTable.java
@@ -3,7 +3,10 @@ package com.TE18C;
 public enum SolidTable {
 
 	ICE(0.92, 2.2, 0, 334),
-	IRON(7.87, 0.45, 1538, 276)
+	IRON(7.87, 0.45, 1538, 276),
+	ALUMINIUM(2.7, 0.9, 660, 397),
+	LEAD(11.3, 0.13, 1064, 63),
+	GOLD(19.3, 0.13, 1064, 63)
 	;
 
 	double density;
