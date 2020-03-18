fdfa46629bf1d5ac179adad2e83f88dcca9c5a98
Rasmus Bengtsson <rasmusb229@gmail.com>
2020-03-18 20:44:43 +0100

    basic shell functionality
    build java 11 Solus Linux

--- talspelet/src/com/company/Shell.java
+++ talspelet/src/com/company/Shell.java
@@ -1,4 +1,42 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Shell {
+
+    private Scanner scanner;
+    private boolean run;
+
+    public Shell() {
+        this.scanner = new Scanner(System.in);
+        this.run = true;
+
+    }
+
+    /**
+     * Starts accepting input from the terminal.
+     */
+    public void initiate(){
+        String[] input;
+        while (run){
+
+            input = acceptInput();
+            if (input == null)  {
+                continue;
+            }
+
+
+        }
+    }
+
+    /**
+     * @return String array the first index being the primary command.
+     */
+    private String[] acceptInput() {
+        String input = null;
+        System.out.println("#");
+        input = scanner.nextLine();
+        return input.split(" ");
+    }
+
 }
